2718ecbb377e436d87017a89b7c0f989402701db
yusukekosaka358 <y.kousaka@4nonome.com>
2022-06-18 19:54:06 +0900

    一回目のコミット

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import item_button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "atoms/button" };
+
+export const button = () => item_button;
