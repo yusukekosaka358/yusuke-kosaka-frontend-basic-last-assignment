21899fe65445d0afd79852b0c15108541764140b
yusukekosaka358 <y.kousaka@4nonome.com>
2022-06-29 23:54:27 +0900

    Revert "コミット取り消し"
    This reverts commit 2718ecbb377e436d87017a89b7c0f989402701db.

--- src/stories/atoms-button.stories.js
+++
@@ -1,6 +0,0 @@
-import item_button from "../components/atoms/button/button.html";
-import "../components/atoms/button/button.scss";
-
-export default { title: "atoms/button" };
-
-export const button = () => item_button;
